1867a5c286754c595917787e0855c9e0afa5b3ef
leon2192 <leon.strokes@gmail.com>
2026-08-02 21:34:30 -0300

    fc

--- src/components/QR.jsx
+++ src/components/QR.jsx
@@ -1,6 +1,5 @@
 import { Box, Typography, Fade } from "@mui/material";
 import { useInView } from "react-intersection-observer";
-import ButtonLinks from "./ButtonLinks/ButtonLInks";
 
 const Qr = () => {
   const { ref, inView } = useInView({
@@ -39,51 +38,19 @@ const Qr = () => {
               mb: 2,
             }}
           >
-            Escanea este QR
+            Escanea este QR para compartir las fotos del evento
           </Typography>
 
           <Box display="flex" justifyContent="center" mb={3}>
             <Box
               component="img"
-              src="/images/qr-placeholder.svg"
+              src="/images/qr.png"
               alt="Código QR"
               sx={{
                 width: { xs: 210, md: 240 },
                 height: { xs: 210, md: 240 },
                 objectFit: "contain",
               }}
-            />
-          </Box>
-
-          <Typography
-            variant="body1"
-            sx={{
-              fontFamily: "var(--font-primary)",
-              fontSize: { xs: "1.2rem", md: "1.5rem" },
-              color: "#000000",
-              mb: 2,
-            }}
-          >
-            Para compartir las fotos del evento!
-          </Typography>
-
-          <Typography
-            variant="body1"
-            sx={{
-              fontFamily: "var(--font-primary)",
-              fontSize: { xs: "1.2rem", md: "1.5rem" },
-              color: "#000000",
-              mb: 2,
-            }}
-          >
-            O puedes presionar aqui
-          </Typography>
-
-          <Box mt={2}>
-            <ButtonLinks
-              label="Ir al album"
-              href="https://photos.google.com/share/AF1QipMF4Niy7oe5VTaE9V4WC7LanXP3kaQVgsRL9nHsI6i5FX5jOpjJKcJxK8JW9Ar8eg?key=V2ZJMWMyZ0hpcHNnbVhmYno1LUpBLTd4bTdYV1pn"
-              newTab
             />
           </Box>
         </Box>
